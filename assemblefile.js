3b39a7e134a5e12c3011ec3657a9ecfc8f1dbdb2
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-22 10:40:54 -0400

    start adding code, docs

---
+++ assemblefile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var assemble = require('assemble');
+var app = assemble();
+
+app.engine('txt', require('engine-base'));
+app.option('engine', 'txt');
+
+app.create('partials');
+app.create('pages');
+
+/**
+ * Data
+ */
+
+app.data({title: 'Site Title'});
+
+/**
+ * Partial
+ */
+
+app.partial('button', {
+  content: 'button: <%= title %>',
+  locals: {title: 'Button Locals Title'},
+  data: {title: 'Button Data Title'}
+});
+
+app.option('context', function(view, locals) {
+  return extend({}, this.cache.data, view.context(), locals);
+});
+
+/**
+ * Page
+ */
+
+app.page('home', {
+  content: 'title: <%= title %>, \n one: <%= partial("button", obj) %>\n two:   <%= partial("button", {title: "Helper Locals Title"}) %>\n three: <%= partial("button") %>',
+  locals: {title: 'Page Locals Title'},
+  data: {title: 'Page Data Title'}
+});
+
+/**
+ * Render
+ */
+
+app.pages.getView('home')
+  .render({title: 'Render Locals Title'}, function(err, res) {
+  // .render({title: 'Render Locals Title'}, function(err, res) {
+    if (err) return console.log(err);
+    console.log(res.content);
+  });
+
