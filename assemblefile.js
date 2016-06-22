5506e04c3e804c37c3e558712c7d6b9f1b2b4ea8
Brian Woodward <brian.woodward@gmail.com>
2016-06-22 14:36:51 -0400

    moving example code to examples
    dynamically creating tasks based on existing examples
    default task to provide instructions on running examples

--- assemblefile.js
+++ assemblefile.js
@@ -1,57 +1,39 @@
 'use strict';
 
+var fs = require('fs');
+var examples = fs.readdirSync('examples');
+
 var assemble = require('assemble');
 var app = assemble();
 
-app.engine('txt', require('engine-base'));
-app.option('engine', 'txt');
-
-app.create('partials');
-app.create('pages');
-
 /**
- * Data
+ * Register each example as a task to make it available to run from the root.
  */
 
-app.data({title: 'Site Title'});
-
-/**
- * Custom `context` function
- */
-
-
-app.option('context', function(view, locals) {
-  return extend({}, this.cache.data, view.context(), locals);
-});
-
-/**
- * Partial (view.locals, view.data)
- */
-
-app.partial('button', {
-  content: 'button: <%= title %>',
-  locals: {title: 'Button Locals Title'},
-  data: {title: 'Button Data Title'}
-});
-
-/**
- * Page
- */
-
-app.page('home', {
-  content: 'title: <%= title %>, \n one: <%= partial("button", obj) %>\n two:   <%= partial("button", {title: "Helper Locals Title"}) %>\n three: <%= partial("button") %>',
-  locals: {title: 'Page Locals Title'},
-  data: {title: 'Page Data Title'}
-});
-
-/**
- * Render
- */
-
-app.pages.getView('home')
-  .render({title: 'Render Locals Title'}, function(err, res) {
-  // .render({title: 'Render Locals Title'}, function(err, res) {
-    if (err) return console.log(err);
-    console.log(res.content);
+examples.forEach(function(example) {
+  app.task(example, {silent: true}, function(cb) {
+    var exampleApp = require('./examples/' + example + '/assemblefile.js');
+    exampleApp.build('default', cb);
   });
+});
 
+/**
+ * List each example that available to run
+ */
+
+app.task('default', {silent: true}, function(cb) {
+  console.log(`
+  Specify an example to run:
+
+    $ assemble <example>
+
+  Available examples:
+
+    ${examples.map(function(example) {
+      return `$ assemble ${example}`;
+    }).join('\n    ')}
+  `);
+  cb();
+});
+
+module.exports = app;
